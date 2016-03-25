3d6441384165e4d531bf868263922ce61c57906f
Wouter Habets <wouterhabets@gmail.com>
2016-03-25 14:14:38 +0100

    Add music control menu items in music fragment

--- app/src/main/java/nl/sjtek/client/android/fragments/FragmentMusic.java
+++ app/src/main/java/nl/sjtek/client/android/fragments/FragmentMusic.java
@@ -1,5 +1,14 @@
 package nl.sjtek.client.android.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nl.sjtek.client.android.R;
 import nl.sjtek.client.android.api.Action;
 import nl.sjtek.client.android.api.InfoRequest;
 import nl.sjtek.client.android.interfaces.OnVolumePressListener;
@@ -8,6 +17,12 @@ import nl.sjtek.client.android.interfaces.OnVolumePressListener;
  * Created by Wouter Habets on 21-10-15.
  */
 public class FragmentMusic extends BaseFragmentWeb implements OnVolumePressListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
     @Override
     protected String getUrl() {
@@ -22,5 +37,24 @@ public class FragmentMusic extends BaseFragmentWeb implements OnVolumePressListe
     @Override
     public void onVolumeLower() {
         addRequest(new InfoRequest(Action.Music.VOLUME_LOWER, this, this));
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_music, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_shuffle:
+                addRequest(new InfoRequest(Action.Music.SHUFFLE, this, this));
+                break;
+            case R.id.action_clear:
+                addRequest(new InfoRequest(Action.Music.CLEAR, this, this));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
