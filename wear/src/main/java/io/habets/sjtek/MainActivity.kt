34a304117415228a05105f6107f2bf0144baa445
Wouter Habets <wouterhabets@gmail.com>
2017-11-12 23:08:24 +0100

    Add wear base

--- wear/src/main/java/io/habets/sjtek/MainActivity.kt
+++ wear/src/main/java/io/habets/sjtek/MainActivity.kt
@@ -2,8 +2,12 @@ package io.habets.sjtek
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import nl.sjtek.control.data.staticdata.Lamp
+import kotlin.concurrent.thread
 
 class MainActivity : WearableActivity() {
+
+    private val lamps: MutableList<Lamp> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,5 +15,19 @@ class MainActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+    }
+
+    private fun refreshLamps() {
+        thread(start = true) {
+            var lamps = Storage.getLamps(this)
+            if (lamps.isEmpty()) {
+                lamps = Net.getLamps(this)
+            }
+            if (!lamps.isEmpty()) {
+                Storage.setLamps(this, lamps)
+                this.lamps.clear()
+                this.lamps.addAll(lamps)
+            }
+        }
     }
 }
